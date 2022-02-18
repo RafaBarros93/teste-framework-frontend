491b82e891e2a6f371efea549e12e1eec9e1190a
rafaeldevfina <rafael@finafacil.com>
2022-02-18 17:06:02 -0300

    Feature: Create simple front end from desáfio.

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,64 @@
-import { useState } from 'react'
+import {useState} from 'react'
+import axios from 'axios'
 import logo from './logo-framework.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [divisibleNumbers, setDivisibleNumbers] = useState([])
+  const [primeNumbers, setPrimeNumbers] = useState([])
 
+  const [number, setNumber] = useState(0)
 
+  const getDivisibleNumbers = async () => {
+    let {data} = await axios.post('http://localhost:3002/api/divisible-number', {number})
 
+    let primeNumbers = data.filter(number => number.primos)
 
+    let divisibleNumbers = data.filter(number => number.divisores)
+
+    setPrimeNumbers(primeNumbers)
+    setDivisibleNumbers(divisibleNumbers)
+  }
+
+  const handlerValue = ({target: {value}}) => {
+    console.log('vale', value)
+
+    setNumber(value)
+  }
+
+  console.log('val::', number)
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-       
-        </p>
-       
+
+        <div>
+          <label>
+            Valor:
+            <input type="number" value={number} onChange={handlerValue} />
+          </label>
+          <button onClick={getDivisibleNumbers}>Gerar </button>
+        </div>
+        {divisibleNumbers.length > 0 && (
+          <div>
+            {divisibleNumbers.map(number => (
+              <div>
+                <ul>Divisores: {number.divisores}</ul>
+              </div>
+            ))}
+          </div>
+        )}
+
+        {primeNumbers.length > 0 && (
+          <div>
+            {primeNumbers.map(number => (
+              <div>
+                <ul>Primos: {number.primos}</ul>
+              </div>
+            ))}
+          </div>
+        )}
       </header>
     </div>
   )
