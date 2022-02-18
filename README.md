dc3c14cd1b826cf26e0cf312cfbbe64eb0b388c7
rafaeldevfina <rafael@finafacil.com>
2022-02-18 15:16:00 -0300

    Feature: First commit

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # teste-framework-frontend
-Frontend do teste para framework feito em React.js
+
+Frontend do teste para framework in [react](https://reactjs.org/)
+
+## Installation
+
+Use the package manager [yarn](https://classic.yarnpkg.com/) to install.
+
+## Install dpendencies
+
+```bash
+yarn
+```
+
+## Run application
+
+```bash
+yarn dev
+
+```
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import logo from './logo-framework.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+
+
+
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Hello Vite + React!</p>
+        <p>
+          <button type="button" onClick={() => setCount((count) => count + 1)}>
+            count is: {count}
+          </button>
+        </p>
+        <p>
+       
+        </p>
+       
+      </header>
+    </div>
+  )
+}
+
+export default App
